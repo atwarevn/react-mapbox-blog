231f3314f566126cc8609d07dc7d571c423fa35a
n-tien <n-tien@atware.asia>
2024-05-06 13:42:49 +0700

    feat: Adding marker and popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import MapboxMap from "./components/MapboxMap.tsx";
 
 function App() {
+  // This can be reactive
+  const points = [
+    {
+      coordinates: { lng: -74.5, lat: 40 },
+      description: "Point A",
+    },
+    {
+      coordinates: { lng: -74.6, lat: 40.1 },
+      description: "Point B",
+    },
+    { coordinates: { lng: -74.4, lat: 39.9 }, description: "Point C" },
+  ];
+
   return (
     <div>
       <h1>My Mapbox App</h1>
-      <MapboxMap />
+      <MapboxMap points={points} />
     </div>
   );
 }
--- src/components/MapboxMap.tsx
+++ src/components/MapboxMap.tsx
@@ -1,17 +1,26 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
-import mapboxgl from "mapbox-gl";
+import mapboxgl, { LngLatLike } from "mapbox-gl";
 
 // Set your Mapbox access token
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamhlbW1pbmdlciIsImEiOiJjbG1uZms3NnQwc2dhMmxwbmN6cnBtMDN5In0.beuY7ZjkZNydgYNkZhIGzw";
 
-const MapboxMap = () => {
+type MapboxMapProps = {
+  readonly points: {
+    readonly coordinates: LngLatLike;
+    readonly description: string;
+  }[];
+};
+
+const MapboxMap = (props: MapboxMapProps) => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
+  const map = useRef<mapboxgl.Map | null>();
+  const [markers, setMarkers] = useState<mapboxgl.Marker[]>([]);
 
   useEffect(() => {
     if (mapContainerRef.current) {
-      const map = new mapboxgl.Map({
+      map.current = new mapboxgl.Map({
         container: mapContainerRef.current,
         style: "mapbox://styles/mapbox/streets-v11",
         center: [-74.006, 40.7128],
@@ -20,13 +29,37 @@ const MapboxMap = () => {
 
       // Cleanup function to remove the map instance on component unmount
       return () => {
-        map.remove();
+        map.current?.remove();
       };
     }
   }, []);
 
+  useEffect(() => {
+    setMarkers(
+      props.points.map((point) => {
+        const popup = new mapboxgl.Popup().setHTML(
+          `<h3>${point.description}</h3>`,
+        );
+        return new mapboxgl.Marker()
+          .setLngLat(point.coordinates)
+          .setPopup(popup);
+      }),
+    );
+  }, [props.points]);
+
+  useEffect(() => {
+    if (map.current) {
+      markers.forEach((marker) => marker.addTo(map.current!));
+    }
+
+    return () =>
+      markers.forEach((marker) => {
+        marker.remove();
+      });
+  }, [markers]);
+
   return (
-    <div ref={mapContainerRef} style={{ height: "400px", width: "100%" }} />
+    <div ref={mapContainerRef} style={{ height: "600px", width: "100%" }} />
   );
 };
 
